b64d0dae9ea7269aa42aeb166cdcf4fd0d1286ee
SabaMaghradze <sabamaghradze1001@gmail.com>
2025-07-10 13:19:05 +0400

    rabbitmq

---
+++ src/main/java/com/dailyhaul/product/controller/MessageController.java
@@ -0,0 +1,19 @@
+package com.dailyhaul.product.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class MessageController { // for just checking whether bus refresh works(if it updates configs)
+
+    @Value("${app.message}") // from product-service.yml
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
+    }
+}
